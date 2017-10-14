c92982bacd019ed542e0694d350e71b9fac35095
Flieris <lenniuch@hotmail.com>
2017-10-14 20:28:22 +0200

    backend work for gathering cell informations

--- app/src/main/java/com/example/sebastian/wirelessmanager/TelephonyFragment.java
+++ app/src/main/java/com/example/sebastian/wirelessmanager/TelephonyFragment.java
@@ -8,12 +8,25 @@ package com.example.sebastian.wirelessmanager;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.telephony.CellIdentityCdma;
+import android.telephony.CellIdentityGsm;
+import android.telephony.CellIdentityLte;
+import android.telephony.CellIdentityWcdma;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoCdma;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
 import android.telephony.CellLocation;
 import android.telephony.TelephonyManager;
+import android.telephony.cdma.CdmaCellLocation;
+import android.telephony.gsm.GsmCellLocation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.List;
 
 
 /**
@@ -22,8 +35,7 @@ import android.widget.TextView;
 public class TelephonyFragment extends Fragment {
     Context mcontext;
     TelephonyManager tm;
-    TextView textView;
-    CellLocation cellLocation;
+    TextView textView, cellView;
     public TelephonyFragment() {
         // Required empty public constructor
     }
@@ -41,6 +53,7 @@ public class TelephonyFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_telephony, container, false);
         mcontext = super.getContext();
         textView=(TextView)view.findViewById(R.id.telephony_tv);
+        cellView=(TextView)view.findViewById(R.id.cell_tv);
         mcontext = super.getContext();
         System.out.println(R.id.telephony_tv);
         tm = (TelephonyManager)mcontext.getSystemService(Context.TELEPHONY_SERVICE);
@@ -52,9 +65,52 @@ public class TelephonyFragment extends Fragment {
         String softwareVersion=tm.getDeviceSoftwareVersion();
         String voiceMailNumber=tm.getVoiceMailNumber();
         String strphoneType="";
-
+        List<CellInfo> cellInfoList = tm.getAllCellInfo();
         int phoneType=tm.getPhoneType();
-
+        String cell_info = "Cell Information:\n";
+        for (CellInfo cellInfos : cellInfoList){
+            if (cellInfos instanceof CellInfoGsm){
+                CellInfoGsm cellInfoGsm = (CellInfoGsm)cellInfos;
+                CellIdentityGsm cellIdentityGsm = cellInfoGsm.getCellIdentity();
+                cell_info += "Cell Id: " + cellIdentityGsm.getCid() + "\n"
+                        + "Mobile Country Code: " + cellIdentityGsm.getMcc() + "\n"
+                        + "Mobile Network Code: " + cellIdentityGsm.getMnc() + "\n"
+                        + "Local Area Code: " + cellIdentityGsm.getLac() + "\n"
+                        + "Signal Str: " + cellInfoGsm.getCellSignalStrength().getDbm() + " [dBm]\n";
+                break;
+            }
+            if (cellInfos instanceof CellInfoCdma){
+                CellInfoCdma cellInfoCdma = (CellInfoCdma) cellInfos;
+                CellIdentityCdma cellIdentityCdma = cellInfoCdma.getCellIdentity();
+                cell_info += "BS Id: " + cellIdentityCdma.getBasestationId() + "\n"
+                        + "Network Id: " + cellIdentityCdma.getNetworkId() + "\n"
+                        + "System Id: " + cellIdentityCdma.getSystemId() + "\n"
+                        + "Latitude: " + cellIdentityCdma.getLatitude()
+                        + " Longitude" + cellIdentityCdma.getLongitude() + "\n";
+                break;
+            }
+            if (cellInfos instanceof CellInfoLte){
+                CellInfoLte cellInfoLte = (CellInfoLte)cellInfos;
+                CellIdentityLte cellIdentityLte = cellInfoLte.getCellIdentity();
+                cell_info += "Cell Id: " + cellIdentityLte.getCi() + "\n"
+                        + "Mobile Country Code: " + cellIdentityLte.getMcc() + "\n"
+                        + "Mobile Network Code: " + cellIdentityLte.getMnc() + "\n"
+                        + "Local Area Code: " + cellIdentityLte.getPci() + "\n"
+                        + "Tracking Area Code: " + cellIdentityLte.getTac() + "\n";
+                break;
+            }
+            if (cellInfos instanceof CellInfoWcdma){
+                CellInfoWcdma cellInfoWcdma = (CellInfoWcdma)cellInfos;
+                CellIdentityWcdma cellIdentityWcdma = cellInfoWcdma.getCellIdentity();
+                cell_info += "Cell Id:" + cellIdentityWcdma.getCid() + "\n"
+                        + "Mobile Country Code: " + cellIdentityWcdma.getMcc() + "\n"
+                        + "Mobile Network Code: " + cellIdentityWcdma.getMnc() + "\n"
+                        + "Local Area Code: " + cellIdentityWcdma.getLac() + "\n"
+                        + "Signal Str: " + cellInfoWcdma.getCellSignalStrength().getDbm() + " [dBm]\n";
+                break;
+            }
+        }
+        cellView.setText(cell_info);
         switch (phoneType)
         {
             case (TelephonyManager.PHONE_TYPE_CDMA):
@@ -78,8 +134,6 @@ public class TelephonyFragment extends Fragment {
         info+="\n Voice Mail Number:"+voiceMailNumber;
         info+="\n Phone Network Type:"+strphoneType;
         info+="\n In Roaming? :"+isRoaming;
-        cellLocation = CellLocation.getEmpty();
-        info+="\n Cell Location :"+cellLocation.toString();
         textView.setText(info);
         return view;
     }
